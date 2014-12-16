b304514500adb8acc927dbf7f095037d72135acb
Kreozot <zzxyyz@gmail.com>
2014-12-16 17:28:28 +0300

    Сделан тайминг Gato Cuyano

--- js/timing.js
+++ js/timing.js
@@ -29,6 +29,24 @@ var music = [
 		title: "Anibal Arias - El Correcto",
 		file: './music/gato_cuyano/el_correcto.mp3',
 		schema: {
+			"#start_1": 4.8,
+			"vuelta": 19,
+			"giro": 26.8,
+			"contraGiro": 30.8,
+			"zapateo": 34.9,
+			"mediaVuelta": 42.9,
+			"zapateo2": 47.3,
+			"coronacion": 55.3,
+			"#pause_2": 59.8,
+			"#start_2": 62.2,
+			"vuelta_2": 70.2,
+			"giro_2": 78.2,
+			"contraGiro_2": 82.2,
+			"zapateo_2": 86.2,
+			"mediaVuelta_2": 94.3,
+			"zapateo2_2": 98.7,
+			"coronacion_2": 106.5,
+			"#end": 110.8
 		}
 	},
 	// Caramba
