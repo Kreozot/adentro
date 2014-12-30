9ba0f250c9bc15bbb31f1ba524f94f62e12c01e8
Kreozot <zzxyyz@gmail.com>
2014-12-31 02:03:00 +0300

    Рефакторинг, документирование кода.

--- js/animations/animation.js
+++ js/animations/animation.js
@@ -7,9 +7,9 @@ function extend(Child, Parent) {
 }
 
 /**
- * [Timer Таймер]
- * @param {Function} callback [Функция]
- * @param {[Number]}   delay    [задержка в мс]
+ * Таймер
+ * @param {Function} callback Функция
+ * @param {Number}   delay    Задержка в мс
  */
 function Timer(callback, delay) {
 	var remaining = delay;
@@ -36,9 +36,9 @@ function Timer(callback, delay) {
 }
 
 /**
- * [getOppositePosition Получение противоположной позиции]
- * @param  {[String]} position [Позиция]
- * @return {[String]}          [Позиция, противоположная указанной]
+ * Получение противоположной позиции
+ * @param  {String} position Позиция
+ * @return {String}          Позиция, противоположная указанной
  */
 function getOppositePosition(position) {
 	switch (position) {
@@ -60,10 +60,10 @@ function getOppositePosition(position) {
 };
 
 /**
- * [SingleDanceAnimationElement одиночная анимация]
- * @param {[Object]} animation   [объект анимации]
- * @param {[String]} pathStrings [строковое описание траекторий в формате [позиция: траектория, ...]]
- * @param {[String]} gender      [пол: "man"; "woman"]
+ * Одиночная анимация
+ * @param {Object} animation   Объект анимации
+ * @param {String} pathStrings Строковое описание траекторий в формате [позиция: траектория, ...]
+ * @param {String} gender      Пол: "man"; "woman"
  */
 function SingleDanceAnimationElement(animation, pathStrings, gender, figure) {
 	this.animation = animation;
@@ -81,8 +81,8 @@ function SingleDanceAnimationElement(animation, pathStrings, gender, figure) {
 
 	
 	/**
-	 * [drawPath отрисовка траектории]
-	 * @param  {[String]} position [позиция]
+	 * Отрисовка траектории
+	 * @param  {String} position Позиция
 	 */
 	this.drawPath = function(position, transparent) {
 		var pathId = this.gender + "_" + position + "_path";
@@ -91,27 +91,27 @@ function SingleDanceAnimationElement(animation, pathStrings, gender, figure) {
 	};
 
 	/**
-	 * [isPathsDrawn Отрисованы ли траектории]
+	 * Отрисованы ли траектории
 	 */
 	this.isPathsDrawn = function() {
 		return this.path.type == "path";
 	};
 
 	/**
-	 * [setAngle Задать относительный угол разворота]
-	 * @param {[Number]} value [значение угла]
+	 * Задать относительный угол разворота
+	 * @param {Number} value Значение угла
 	 */
 	this.setAngle = function(value) {
 		this.angle = value;
 	};
 
 	/**
-	 * [animationFunction инициализация анимации]
-	 * @param  {[Number]} lengthMs  [длина в милисекундах]
-	 * @param  {[Number]} times     [количество тактов]
-	 * @param  {[String]} direction [направление движения]
-	 * @param  {[Number]} startPart [позиция начала (0-1 относительно траектории)]
-	 * @param  {[Number]} stopPart  [позиция конца (0-1 относительно траектории)]
+	 * Инициализация анимации
+	 * @param  {Number} lengthMs  Длина в милисекундах
+	 * @param  {Number} times     Количество тактов
+	 * @param  {String} direction Направление движения
+	 * @param  {Number} startPart Позиция начала (0-1 относительно траектории)
+	 * @param  {Number} stopPart  Позиция конца (0-1 относительно траектории)
 	 */
 	this.animationFunction = function(lengthMs, times, direction, startPart, stopPart) {
 		this.animation.animateFigurePath(this.figure, 90 + this.angle, this.path, 
@@ -119,13 +119,13 @@ function SingleDanceAnimationElement(animation, pathStrings, gender, figure) {
 	};
 
 	/**
-	 * [startAnimation Запуск анимации]
-	 * @param  {[Number]} lengthS   [длительность в секундах]
-	 * @param  {[Number]} times     [количество тактов]
-	 * @param  {[String]} direction [направление движения фигуры]
-	 * @param  {[Number]} delay     [задержка в секундах]
-	 * @param  {[Number]} startPart [позиция начала (0-1 относительно траектории)]
-	 * @param  {[Number]} stopPart  [позиция конца (0-1 относительно траектории)]
+	 * Запуск анимации
+	 * @param  {Number} lengthS   Длительность в секундах
+	 * @param  {Number} times     Количество тактов
+	 * @param  {String} direction Направление движения фигуры
+	 * @param  {Number} delay     Задержка в секундах
+	 * @param  {Number} startPart Позиция начала (0-1 относительно траектории)
+	 * @param  {Number} stopPart  Позиция конца (0-1 относительно траектории)
 	 */
 	this.startAnimation = function(lengthS, times, direction, delay, startPart, stopPart) {
 		startPart = (typeof startPart === 'undefined') ? 0 : startPart;
@@ -145,11 +145,11 @@ function SingleDanceAnimationElement(animation, pathStrings, gender, figure) {
 
 	/**
 	 * [fullAnimation Полный цикл анимации]
-	 * @param  {[Number]} lengthS    	[длительность в секундах]
-	 * @param  {[Number]} times     	[количество тактов]
-	 * @param  {[String]} manPosition   [начальная позиция партнёра]
-	 * @param  {[String]} direction 	[направление движения фигуры]
-	 * @param  {[Number]} delay     	[задержка в секундах]
+	 * @param  {Number} lengthS    	[длительность в секундах]
+	 * @param  {Number} times     	[количество тактов]
+	 * @param  {String} manPosition   [начальная позиция партнёра]
+	 * @param  {String} direction 	[направление движения фигуры]
+	 * @param  {Number} delay     	[задержка в секундах]
 	 */
 	this.fullAnimation = function(lengthS, times, position, direction, delay, startPart, stopPart) {
 		var self = this;
@@ -168,9 +168,9 @@ function SingleDanceAnimationElement(animation, pathStrings, gender, figure) {
 }
 
 /**
- * [DanceAnimationElement парная анимация]
- * @param {[Object]} animation   [объект анимации]
- * @param {[Object]} pathStrings [строковое описание траекторий в формате [позиция: траектория, ...]]
+ * Парная анимация
+ * @param {Object} animation   Объект анимации
+ * @param {Object} pathStrings Строковое описание траекторий в формате позиция: траектория, ...
  */
 function DanceAnimationElement(animation, pathStrings) {
 	this.animation = animation;
@@ -178,8 +178,8 @@ function DanceAnimationElement(animation, pathStrings) {
 	this.womanDanceAnimationElement = new SingleDanceAnimationElement(animation, pathStrings, "woman");
 	// TODO: Сделать массив с элементами и оперировать циклами, а не отдельными элементами
 	/**
-	 * [drawPath отрисовка траекторий]
-	 * @param  {[String]} manPosition [позиция партнёра]
+	 * Отрисовка траекторий
+	 * @param  {String} manPosition Позиция партнёра
 	 */
 	this.drawPath = function(manPosition, transparent) {
 		this.animation.manPosition = manPosition;
@@ -188,15 +188,15 @@ function DanceAnimationElement(animation, pathStrings) {
 	};
 
 	/**
-	 * [isPathsDrawn Отрисованы ли траектории]
+	 * Отрисованы ли траектории
 	 */
 	this.isPathsDrawn = function() {
 		return (this.manDanceAnimationElement.isPathsDrawn() && this.womanDanceAnimationElement.isPathsDrawn());
 	};
 
 	/**
-	 * [setAngle Задать относительный угол разворота]
-	 * @param {[Number]} value [значение угла]
+	 * Задать относительный угол разворота
+	 * @param {Number} value Значение угла
 	 */
 	this.setAngle = function(value) {
 		this.manDanceAnimationElement.setAngle(value);
@@ -204,9 +204,9 @@ function DanceAnimationElement(animation, pathStrings) {
 	};
 
 	/**
-	 * [setAngle Задать относительный углов разворота]
-	 * @param {[Number]} manValue [значение угла для партнёра]
-	 * @param {[Number]} womanValue [значение угла для партнёрши]
+	 * Задать относительный углов разворота
+	 * @param {Number} manValue   Значение угла для партнёра
+	 * @param {Number} womanValue Значение угла для партнёрши
 	 */
 	this.setAngles = function(manValue, womanValue) {
 		this.manDanceAnimationElement.setAngle(manValue);
@@ -214,13 +214,13 @@ function DanceAnimationElement(animation, pathStrings) {
 	};
 
 	/**
-	 * [startAnimation Запуск анимации]
-	 * @param  {[Number]} lengthS   [длительность в секундах]
-	 * @param  {[Number]} times     [количество тактов]
-	 * @param  {[String]} direction [направление движения фигуры]
-	 * @param  {[Number]} delay     [задержка в секундах]
-	 * @param  {[Number]} startPart [позиция начала (0-1 относительно траектории)]
-	 * @param  {[Number]} stopPart  [позиция конца (0-1 относительно траектории)]
+	 * Запуск анимации
+	 * @param  {Number} lengthS   Длительность в секундах
+	 * @param  {Number} times     Количество тактов
+	 * @param  {String} direction Направление движения фигуры
+	 * @param  {Number} delay     Задержка в секундах
+	 * @param  {Number} startPart Позиция начала (0-1 относительно траектории)
+	 * @param  {Number} stopPart  Позиция конца (0-1 относительно траектории)
 	 */
 	this.startAnimation = function(lengthS, times, direction, delay, startPart, stopPart) {
 		this.manDanceAnimationElement.startAnimation(lengthS, times, direction, delay, startPart, stopPart);
@@ -228,12 +228,12 @@ function DanceAnimationElement(animation, pathStrings) {
 	};
 
 	/**
-	 * [fullAnimation Полный цикл анимации]
-	 * @param  {[Number]} lengthS    	[длительность в секундах]
-	 * @param  {[Number]} times     	[количество тактов]
-	 * @param  {[String]} manPosition   [начальная позиция партнёра]
-	 * @param  {[String]} direction 	[направление движения фигуры]
-	 * @param  {[Number]} delay     	[задержка в секундах]
+	 * Полный цикл анимации
+	 * @param  {Number} lengthS    	Длительность в секундах
+	 * @param  {Number} times     	Количество тактов
+	 * @param  {String} manPosition Начальная позиция партнёра
+	 * @param  {String} direction 	Направление движения фигуры
+	 * @param  {Number} delay     	Задержка в секундах
 	 */
 	this.fullAnimation = function(lengthS, times, manPosition, direction, delay, startPart, stopPart) {
 
@@ -255,6 +255,101 @@ function DanceAnimationElement(animation, pathStrings) {
 	};
 }
 
+/**
+ * Продвижение с вращением
+ * @param {Object} animation   Объект анимации
+ * @param {String} pathStrings Строковое описание траекторий в формате [позиция: траектория, ...]
+ * @param {String} gender      Пол: "man"; "woman"
+ * @param {Object} figure      Объект фигуры анимации
+ * @param {Number} rotateAngle Угол поворота
+ */
+function RotateElement(animation, pathStrings, gender, figure, rotateAngle) {
+	RotateElement.superclass.constructor.apply(this, arguments);
+
+	this.rotateAngle = rotateAngle;
+
+	this.animationFunction = function(lengthMs, times) {
+		this.animation.startPosFigure(this.figure, this.animation.startPos[this.position]);
+		this.animation.animateFigureTime(this.figure, lengthMs, times * 6);
+	}
+
+	this.startAnimation = function(lengthS, times, startAngle, direction, delay, startPart, stopPart) {
+		startPart = (typeof startPart === 'undefined') ? 0 : startPart;
+		stopPart = (typeof stopPart === 'undefined') ? 1 : stopPart;
+		var self = this;
+		function startAnimationFunc() {
+			var angle = startAngle;
+			var startLen = self.pathLength * startPart;
+			var stopLen = self.pathLength * stopPart;
+			var angleSpeed = rotateAngle / (stopLen - startLen);
+
+			function transformAtLength(length) {
+				movePoint = self.path.getPointAtLength(length);
+				self.animation.positionFigure(self.figure, movePoint.x, movePoint.y, angle - angleSpeed * (length - startLen));
+			}
+
+			transformAtLength(startLen);
+			self.figure.removeClass("invisible");
+
+			self.animation.animations[self.animation.animations.length] = Snap.animate(startLen, stopLen, 
+				function(value) {
+					this.lastValue = value;
+					transformAtLength(value);
+				}, lengthS * 1000, mina.linear);
+
+			self.animation.animateFigureTime(self.figure, lengthS * 1000, times);		
+		}
+
+		if ((!delay) || (delay <= 0)) {
+			startAnimationFunc();		
+		} else {
+			this.animation.timeouts[this.animation.timeouts.length] = new Timer(startAnimationFunc, delay * 1000);
+		}
+	};
+}
+extend(RotateElement, SingleDanceAnimationElement);
+
+/**
+ * Парная анимация для продвижения с вращением
+ * @param {Object} animation   Объект анимации
+ * @param {String} pathStrings Строковое описание траекторий в формате [позиция: траектория, ...]
+ * @param {Number} rotateAngle Угол поворота
+ */
+function RotateDanceAnimationElement(animation, pathStrings, rotateAngle) {
+	RotateDanceAnimationElement.superclass.constructor.apply(this, arguments);
+	this.manDanceAnimationElement = new RotateElement(animation, pathStrings, "man", undefined, rotateAngle);
+	this.womanDanceAnimationElement = new RotateElement(animation, pathStrings, "woman", undefined, rotateAngle);
+
+	this.startAnimation = function(lengthS, times, startAngleMan, startAngleWoman, direction, delay, startPart, stopPart) {
+		this.manDanceAnimationElement.startAnimation(lengthS, times, startAngleMan, direction, delay, startPart, stopPart);
+		this.womanDanceAnimationElement.startAnimation(lengthS, times, startAngleWoman, direction, delay, startPart, stopPart);
+	};
+
+	this.fullAnimation = function(lengthS, times, startAngleMan, startAngleWoman, manPosition, direction, delay, startPart, stopPart) {
+
+		this.fullAnimationFunc = function() {
+			this.animation.clearPaths();
+			this.drawPath(manPosition);
+			this.startAnimation(lengthS, times, startAngleMan, startAngleWoman, direction, 0, startPart, stopPart);		
+		}
+
+		if ((!delay) || (delay <= 0)) {
+			this.fullAnimationFunc();
+		} else {
+			this.animation.timeouts[this.animation.timeouts.length] = new Timer(function(self) {
+				return function() {					
+					self.fullAnimationFunc();
+				};
+			}(this), delay * 1000);
+		}
+	};
+}
+extend(RotateDanceAnimationElement, DanceAnimationElement);
+
+/**
+ * Объект анимации
+ * @param {String} id DOM-идентификатор SVG-объекта
+ */
 function DanceAnimation(id) {
 	this.svg = Snap("#" + id);
 
