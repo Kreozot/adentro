1b2e161ac7fae114d5a31b50eadd202fb3638784
Kreozot <zzxyyz@gmail.com>
2015-01-03 11:25:13 +0300

    Исправление работы с пробелами при минимизации HTML

--- Gruntfile.js
+++ Gruntfile.js
@@ -109,7 +109,7 @@ module.exports = function(grunt) {
 			build: {
 				options: {
 					removeComments: true,
-					collapseWhitespace:true,
+					collapseWhitespace: false,
 					minifyJS: true
 				},
 				files: [
