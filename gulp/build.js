6e5a13b831fc1162b27fd2e0ff298e800b304478
Kreozot <zzxyyz@gmail.com>
2016-01-04 22:20:25 +0300

    PostHTML

--- gulp/build.js
+++ gulp/build.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['build-js', 'copy-html']);
+gulp.task('build', ['build-js', 'process-html']);
--- gulp/html.js
+++ gulp/html.js
@@ -2,16 +2,35 @@ var gulp = require('gulp');
 var config = require('./config.js');
 var paths = config.paths;
 var del = require('promised-del');
+var posthtml = require('gulp-posthtml');
+var posthtmlInclude = require('posthtml-include');
+var posthtmlDoctype = require('posthtml-doctype');
+var posthtmlExtendAttrs = require('posthtml-extend-attrs');
+var version = require('../package.json').version;
 
-gulp.task('clean-html', function() {
+gulp.task('clean-html', function () {
     return del(config.paths.dist.html + '/*.html');
 });
 
-gulp.task('copy-html', ['clean-html'], function () {
-	return gulp.src(paths.src.html + '/*.html')
-		.pipe(gulp.dest(paths.dist.html));
+gulp.task('process-html', ['clean-html'], function () {
+    return gulp.src(paths.src.html + '/*.html')
+        .pipe(posthtml([
+        	posthtmlInclude({
+        		root: paths.src.html,
+        		encoding: 'utf-8'
+        	}),
+        	posthtmlDoctype({doctype: 'HTML 5'}),
+        	posthtmlExtendAttrs({
+        		attrsTree: {
+        			'.version': {
+        				'data-version': version
+        			}
+        		}
+        	})
+        ]))
+        .pipe(gulp.dest(paths.dist.html));
 });
 
-gulp.task('watch-html', ['connect', 'copy-html'], function() {
-    return gulp.watch(paths.src.html + '/*.html', ['copy-html']);
+gulp.task('watch-html', ['connect', 'process-html'], function () {
+    return gulp.watch(paths.src.html + '/*.html', ['process-html']);
 });
