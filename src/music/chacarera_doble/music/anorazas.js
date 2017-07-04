a41a78c1995fee32e2ac0ee7b1d99e0aa229757d
Kreozot <zzxyyz@gmail.com>
2017-07-04 10:44:24 +0300

    doble on 6 fix

--- src/music/chacarera_doble/music/anorazas.js
+++ src/music/chacarera_doble/music/anorazas.js
@@ -3,5 +3,11 @@ module.exports = {
 	id: 'chacarera_doble_6_anorazas',
 	title: 'Anorazas (6)',
 	file: require('./anorazas.mp3'),
-	schema: require('./anorazas.yaml')
+	schema: require('./anorazas.yaml'),
+	schemeMods: {
+		vuelta: {times: 6},
+		vuelta2: {times: 6},
+		vuelta_2: {times: 6},
+		vuelta2_2: {times: 6}
+	}
 };
--- src/music/chacarera_doble/music/pampa_de_los_guanacos.js
+++ src/music/chacarera_doble/music/pampa_de_los_guanacos.js
@@ -3,5 +3,11 @@ module.exports = {
 	id: 'chacarera_doble_6_pampa_de_los_guanacos',
 	title: 'Los Reynoso — Pampa de los guanacos (6)',
 	file: require('./pampa_de_los_guanacos.mp3'),
-	schema: require('./pampa_de_los_guanacos.yaml')
+	schema: require('./pampa_de_los_guanacos.yaml'),
+	schemeMods: {
+		vuelta: {times: 6},
+		vuelta2: {times: 6},
+		vuelta_2: {times: 6},
+		vuelta2_2: {times: 6}
+	}
 };
