1b29427f431ba7e567706bc29045f5223655120f
Kreozot <zzxyyz@gmail.com>
2017-08-16 11:47:14 +0300

    Remedio paths extracted to svg

--- src/js/animations/svg/svg.js
+++ src/js/animations/svg/svg.js
@@ -4,3 +4,4 @@ export const huayraMuyoj = getSvgPaths('huayra_muyoj.svg');
 export const vuelta = getSvgPaths('vuelta.svg');
 export const bailecito = getSvgPaths('bailecito.svg');
 export const zamba = getSvgPaths('zamba.svg');
+export const remedio = getSvgPaths('remedio.svg');
