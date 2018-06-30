848476e60628222fdece30ca9bde6fddb63e7fdc
Sergey Sharov <s.sharov@zebra-group.ru>
2018-06-30 12:35:57 +0300

    mp3 mock task fix

--- gulp/mockMp3.js
+++ gulp/mockMp3.js
@@ -1,41 +1,35 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var del = require('promised-del');
-var mergeStream = require('merge-stream');
-var config = require('./config.js');
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const del = require('promised-del');
+const mergeStream = require('merge-stream');
+const config = require('./config.js');
+const Promise = require('bluebird');
+const readdir = require("recursive-readdir");
+const path = require('path');
+const fs = Promise.promisifyAll(require('fs'));
 
 /**
  * Получение списка mp3-композиций для подмены
  * @return {Promise} Promise, возвращающий массив объектов {dir, file}
  */
 function getMp3List() {
-	var mp3Regexp = /require\('mp3Files\/([\S]+)\/([\S]+.mp3)'\)/;
+	const mp3Regexp = /require\('.\/([\S]+.mp3)'\)/;
+	const jsRegexp = /\.js$/;
 
-	return fs.readdirAsync(config.paths.src.musicData)
-		// Получаем список полных путей файлов описания композиций
-		.map(function (filename) {
-			return config.paths.src.musicData + '/' + filename;
-		})
-		// Фильтруем только то, что является файлами
-		.filter(function (filepath) {
-			return fs.statAsync(filepath)
-				.then(function (stat) {
-					return stat.isFile();
-				});
-		})
+	return Promise.resolve(readdir(config.paths.src.musicData))
+		// Получаем все js-файлы в папке с музыкой
+		.filter((filename) => jsRegexp.test(filename))
 		// Получаем из файлов описания названия файлов и папок композиций
-		.map(function (filepath) {
-			return fs.readFileAsync(filepath)
-				.then(function (data) {
-					var match = mp3Regexp.exec(data);
-					return {
-						dir: match[1],
-						file: match[2]
-					};
-				});
-		});
+		.map((jsFilepath) => fs.readFileAsync(jsFilepath)
+			.then((buffer) => String(buffer))
+			.then((data) => {
+				const match = mp3Regexp.exec(data);
+				const dirname = path.dirname(jsFilepath);
+				return match ? path.join(dirname, match[1]) : null;
+			})
+		)
+		// Убираем пустые вхождения
+		.filter((filepath) => filepath);
 }
 
 gulp.task('clean-mp3-mock', function () {
@@ -44,13 +38,13 @@ gulp.task('clean-mp3-mock', function () {
 
 gulp.task('mock-mp3', ['clean-mp3-mock'], function () {
 	return getMp3List()
-		.then(function (mp3List) {
-			var merged = mergeStream();
-			mp3List.forEach(function (mp3) {
+		.then((mp3List) => {
+			const merged = mergeStream();
+			mp3List.forEach((mp3Filepath) => {
 				merged.add(
 					gulp.src(config.files.mockMp3)
-						.pipe(rename(mp3.file))
-						.pipe(gulp.dest(config.paths.temp.mp3Mock + '/' + mp3.dir))
+						.pipe(rename(path.relative(config.paths.src.musicData, mp3Filepath)))
+						.pipe(gulp.dest(config.paths.src.musicData))
 				);
 			});
 			return merged;
