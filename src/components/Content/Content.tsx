d06017e03c479a06673e4d85c4c76b8c2d341671
Kreozot <zzxyyz@gmail.com>
2021-07-11 19:41:58 +0300

    Content container styles

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+import { MEDIA } from 'styles';
+
+const ContentContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+
+  ${MEDIA.M} {
+    margin-top: 64px;
+  }
+
+  ${MEDIA.S} {
+    margin-top: 48px;
+  }
+`;
 
 const Content = () => {
   return (
-    <div>Content</div>
+    <ContentContainer>Content</ContentContainer>
   );
 };
 export default Content;
